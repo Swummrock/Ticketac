efc324301a601626c0db39b68c4b4cd92013dba3
Donka Prat <donkaprat@MacBook-Pro-de-Donka.local>
2020-02-07 10:52:45 +0100

    gavroche commit

--- routes/index.js
+++ routes/index.js
@@ -9,35 +9,85 @@ var userModel = require('../models/users')
 var city = ["Paris", "Marseille", "Nantes", "Lyon", "Rennes", "Melun", "Bordeaux", "Lille"]
 var date = ["2018-11-20", "2018-11-21", "2018-11-22", "2018-11-23", "2018-11-24"]
 
-
-
 // --------------------- BDD -----------------------------------------------------
 
-
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/', function (req, res, next) {
   res.render('login');
 });
 
-router.get('/ticketac', async function (req, res, next) {
-  if (req.session.user == null) {
-    res.redirect('/')
-  } else {
-    var journeyList = await journeyModel.find();
+router.post('/sign-up', async function (req, res, next) {
 
-    res.render('ticketac', { journeyList })
+  var searchUser = await userModel.findOne({
+    email: req.body.email
+  })
+  console.log(searchUser)
+
+  if (!searchUser) {
+    var newUser = new userModel({
+      name: req.body.name,
+      firstname: req.body.firstname,
+      email: req.body.email,
+      password: req.body.password,
+    })
+
+var newUserSave = await newUser.save();
+console.log(newUserSave)
+
+req.session.user = {
+  name: newUserSave.name,
+  id: newUserSave._id,
+}
+
+res.redirect('/ticetac')
+  } else {
+    res.redirect('/')
+  }
+
+})
+
+router.get('/ticetac', async function (req, res, next) {
+  // if (req.session.user == null) {
+  //   res.redirect('/')
+  // } else {
+  //   var journeyList = await journeyModel.find();
+
+  res.render('ticetac', )
+  // }
+})
+
+router.post('/sign-in', async function (req, res, next) {
+
+  var searchUser = await userModel.findOne({
+    email: req.body.email,
+    password: req.body.password
+  })
+
+  if (searchUser != null) {
+    req.session.user = {
+      email: searchUser.email,
+      password: searchUser.password
+    }
+    res.redirect('/ticetac')
+  } else {
+    res.render('login')
   }
 })
 
 router.get('/available', async function (req, res, next) {
-  var journeyList = await journeyModel.find();
+  var depart = req.body.departCity;
+  console.log(depart)
+  var arrival = req.body.arrivalCity;
+  var date = req.body.chooseDate;
+  console.log(arrival);
+  console.log(date);
+
+  var journeyList = await journeyModel.find({
+    departure: req.body.departCity,
+    arrival: req.body.arrivalCity
+  })
+  console.log(journeyList)
 
   for (var i = 0; i < journeyList.length; i++) {
-
     await journeyModel.updateOne({
       _id: journeyList[i].id
     }, {
@@ -47,13 +97,19 @@ router.get('/available', async function (req, res, next) {
       departureTime: journeyList[i].departureTime,
       price: journeyList[i].price,
     })
-  }
-
-  var journeyList = await journeyModel.find();
-
+  } 
   res.render('available', { journeyList })
+
+  // else {
+  //   res.redirect('/oops')
+  // }
+
 })
 
+router.get('/oops', async function (req, res, next) {
+  res.render('oops', )
+
+})
 
 // Remplissage de la base de donnée, une fois suffit
 router.get('/save', async function(req, res, next) {
@@ -64,27 +120,25 @@ router.get('/save', async function(req, res, next) {
   // Save  ---------------------------------------------------
     for(var i = 0; i< count; i++){
 
-    departureCity = city[Math.floor(Math.random() * Math.floor(city.length))]
-    arrivalCity = city[Math.floor(Math.random() * Math.floor(city.length))]
+departureCity = city[Math.floor(Math.random() * Math.floor(city.length))]
+arrivalCity = city[Math.floor(Math.random() * Math.floor(city.length))]
 
-    if(departureCity != arrivalCity){
+if(departureCity != arrivalCity){
 
-      var newUser = new journeyModel ({
-        departure: departureCity , 
-        arrival: arrivalCity, 
-        date: date[Math.floor(Math.random() * Math.floor(date.length))],
-        departureTime:Math.floor(Math.random() * Math.floor(23)) + ":00",
-        price: Math.floor(Math.random() * Math.floor(125)) + 25,
-      });
-       
-       await newUser.save();
+  var newUser = new journeyModel ({
+    departure: departureCity , 
+    arrival: arrivalCity, 
+    date: date[Math.floor(Math.random() * Math.floor(date.length))],
+    departureTime:Math.floor(Math.random() * Math.floor(23)) + ":00",
+    price: Math.floor(Math.random() * Math.floor(125)) + 25,
+  });
+   
+   await newUser.save();
 
-    }
-
+}
   }
   res.render('index', { title: 'Express' });
 });
-
 
 // Cette route est juste une verification du Save.
 // Vous pouvez choisir de la garder ou la supprimer.
@@ -93,17 +147,15 @@ router.get('/result', function(req, res, next) {
   // Permet de savoir combien de trajets il y a par ville en base
   for(i=0; i<city.length; i++){
 
-    journeyModel.find( 
-      { departure: city[i] } , //filtre
+journeyModel.find( 
+  { departure: city[i] } , //filtre
   
-      function (err, journey) {
+  function (err, journey) {
 
-          console.log(`Nombre de trajets au départ de ${journey[0].departure} : `, journey.length);
-      }
-    )
-
+      console.log(`Nombre de trajets au départ de ${journey[0].departure} : `, journey.length);
   }
-
+)
+  }
 
   res.render('index', { title: 'Express' });
 });
